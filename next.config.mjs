2c3d03964097bc9e6af8b83728dd260977d58bf4
chisangainnocent <chisangainnocent3477@gmail.com>
2025-08-12 11:17:18 +0200

    ANOTHER URL

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    allowedDevOrigins: ['local-origin.dev', '*.local-origin.dev','10.171.69.211'],
+    allowedDevOrigins: ['local-origin.dev', '*.local-origin.dev','10.171.69.211','https://fundflow-ao6z.onrender.com'],
     
 };
 
